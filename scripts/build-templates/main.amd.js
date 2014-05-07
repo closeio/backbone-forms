626208e06d611ad0bc6b476d4d05edec7f2fa886
Phil Freo <phil@philfreo.com>
2014-05-07 16:00:12 -0700

    Requiring 'backbone-options-patch' which patches Backbone so older backbone-forms are compatible with newer backbones.
    contents of file are:
    
    define([
        'backbone'
    ],
    
    function(Backbone) {
        // TODO we should get rid of this file after we upgrade Backbone-Forms to v0.13.0+
    
        // Compatibility override - Backbone 1.1 got rid of the 'options' binding
        // automatically to views in the constructor - we need to keep that.
        Backbone.View = (function(View) {
           return View.extend({
                constructor: function(options) {
                    this.options = options || {};
                    View.apply(this, arguments);
                }
            });
        })(Backbone.View);
    
        return Backbone;
    });

--- scripts/build-templates/main.amd.js
+++ scripts/build-templates/main.amd.js
@@ -10,7 +10,7 @@
  * License and more information at:
  * http://github.com/powmedia/backbone-forms
  */
-define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
+define(['jquery', 'underscore', 'backbone', 'backbone-options-patch'], function($, _, Backbone) {
 
   {{body}}
 
